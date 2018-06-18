ec311aa384fc4f4d0d1652b3c1e606863947929e
jiena76 <jieun@purdue.edu>
2018-06-18 14:30:35 -0400

    add Github.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Route, Switch} from "react-router-dom";
 
 import './App.css';
+import Github from './Github'
 
 class App extends Component {
   render() {
