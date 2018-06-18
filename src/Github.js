8191a4ab60577e206b2a5bd29168e00937e0d72a
jiena76 <jieun@purdue.edu>
2018-06-18 14:51:08 -0400

    add github.css

--- src/Github.js
+++ src/Github.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
 
 class Github extends Component{
+
+  state={
+    username: "",
+  }
+
+  handleChange = (e) => {
+    this.setState({username: ev.target.value});
+  }
+
   render(){
     return(
       <div className="Github">
-        
+        <img
+          src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png"
+          alt="GitHub"
+          className="logo"
+        />
+
+        <form>
+          <div>
+            <input 
+              type="text"
+              placeholder="Enter a GitHub user"
+            />
+          </div>
+          <button type="submit">Look up GitHub user</button>
+        </form>
+
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+
 ReactDOM.render(
   <Router>
     <App />
